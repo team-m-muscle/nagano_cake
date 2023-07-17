cfb6d80f5ab8d2e6441af2befd43668efab855a4
EC2 Default User <ec2-user@ip-172-31-1-76.ap-northeast-1.compute.internal>
2023-07-17 07:45:09 +0000

    [add]item, genre models,controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_17_041425) do
+ActiveRecord::Schema.define(version: 2023_07_17_053706) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -40,6 +40,22 @@ ActiveRecord::Schema.define(version: 2023_07_17_041425) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "genre_id"
+    t.string "name", null: false
+    t.text "explanation", null: false
+    t.integer "price", null: false
+    t.boolean "is_active", default: true, null: false
   end
 
   create_table "users", force: :cascade do |t|
