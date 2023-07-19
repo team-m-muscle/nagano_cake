e5451950b8bd91140e9317e12d2fa97ac24c88d7
hamtos <135288715+hamtos@users.noreply.github.com>
2023-07-19 06:34:45 +0000

    [add]item_form

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,4 +1,20 @@
 class Admin::ItemsController < ApplicationController
   def index
+    items = Item.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
   end
 end
