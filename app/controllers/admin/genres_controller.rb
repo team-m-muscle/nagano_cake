c5d1e0d96184a9d5bb6efc5411a3e6da1ce24328
EC2 Default User <ec2-user@ip-172-31-36-52.ap-northeast-1.compute.internal>
2023-07-20 07:12:43 +0000

    genre loyout

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,21 +1,21 @@
 class Admin::GenresController < ApplicationController
-  
+
   def index
     @genre = Genre.new
     @genres = Genre.all
-  end 
-  
+  end
+
   def create
     @genre = Genre.new(genre_params)
     @genres = Genre.all
     @genre.save
-    render :index
+    redirect_to admin_genres_path
   end
-  
+
   def edit
     @genre = Genre.find(params[:id])
-  end 
-  
+  end
+
   def update
     @genre = Genre.find(params[:id])
     if @genre.update(genre_params)
@@ -23,13 +23,13 @@ class Admin::GenresController < ApplicationController
     else
       render :edit
     end
-  end  
-  
-  
+  end
+
+
    private
-  
+
   def genre_params
     params.require(:genre).permit(:name)
   end
-  
+
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,8 @@
 class Genre < ApplicationRecord
-  
+
+  validates :name, presence: :true
+
   has_many :items, dependent: :destroy
-  
+
+
 end
