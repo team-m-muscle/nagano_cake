9feed5eac873508184a6d96d54d3b5bb9abdadd8
hamtos <135288715+hamtos@users.noreply.github.com>
2023-07-20 08:30:18 +0000

    [add]feature_item_edit

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -14,12 +14,17 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_item_path(@item)
   end
 
   private
