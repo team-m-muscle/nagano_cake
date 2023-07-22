381e45e7719930c88088026f95f1877d31625f02
hamtos <135288715+hamtos@users.noreply.github.com>
2023-07-22 13:09:30 +0000

    [add]order_confirm

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,12 +1,30 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.save
   end
 
   def confirm
     @cart_items = CartItem.where(customer_id: current_customer.id)
+    params[:order][:payment_option] = params[:order][:payment_option].to_i
+    @order = Order.new(order_params)
+    case params[:order][:select_address]
+      when "0"
+        @order.name = current_customer.last_name + current_customer.first_name
+        @order.address = current_customer.address
+        @order.post_code = current_customer.post_code
+      when "1"
+        @address = Address.find(params[:order][:address_id])
+        @order.name = @address.name
+        @order.address = @address.address
+        @order.post_code = @address.post_code
+      when "2"
+    end
+
   end
 
   def complete
@@ -19,5 +37,7 @@ class Public::OrdersController < ApplicationController
   end
 
   private
-
+  def order_params
+    params.require(:order).permit(:customer_id, :shipping_fee, :total_price, :payment_option, :address, :post_code, :name)
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,11 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items, dependent: :destroy
 
+  enum payment_option: { credit_card: 0, transfer: 1 }
+  enum status: { payment_waiting: 0, payment_confirmation: 1, in_production: 2,  preparing_delivery: 3, delivered: 4}
+
+  def address_display
+    '〒' + post_code + ' ' + address + ' ' + name
+  end
+
 end
