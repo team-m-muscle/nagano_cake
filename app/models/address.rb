45b3ede60df48b12118b19298b702dc30aeb9bc9
hamtos <135288715+hamtos@users.noreply.github.com>
2023-07-23 01:05:28 +0000

    [fix]

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,14 +1,12 @@
 class Address < ApplicationRecord
   belongs_to :customer
 
-<<<<<<< HEAD
   def address_display
     '〒' + post_code + ' ' + address + ' ' + name
   end
-=======
+
   validates :name, presence:true
   validates :address, presence:true
   validates :post_code, presence:true, length: { is: 7 }, numericality: true
 
->>>>>>> develop
 end
