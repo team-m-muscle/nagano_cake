984cba11de04581d0b94777b45a8ed75c701e8e0
EC2 Default User <ec2-user@ip-172-31-36-52.ap-northeast-1.compute.internal>
2023-07-24 08:53:43 +0000

    items_index

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,9 +1,47 @@
 class Public::CartItemsController < ApplicationController
 
   def index
+    @cart_items = current_customer.cart_items
+    @items = Item.all
+    @cart_item = @items.all
+    @total_price = 0
+    
+  end
+  
+  
+  
+  def create
+    @cart_item = CartItem.find_by(customer_id: current_customer.id, item_id: params[:cart_item][:item_id])
+    if @cart_item
+      @cart_item.quantity = CartItem.new(cart_item_params).quantity
+    else
+      @cart_item = CartItem.new(cart_item_params)
+    end
+    @cart_item.customer_id = current_customer.id
+    @cart_item.save
+    redirect_to cart_items_path
   end
 
-  def show
+  def update
+    @cart_item = CartItem.find(params[:id])
+    @item = Item.all
+    @cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
 
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
+  end
+
+  def destroy_all
+    current_customer.cart_items.destroy_all
+    redirect_to cart_items_path
+  end
+
+  private
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :quantity)
+  end   
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,9 @@
 class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
+  
+ def subtotal_price
+   items.price * quantity
+ end
+ 
 end
